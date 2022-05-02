c5496611593ac63a1dd23bf5c12cdaaa24e3eed9
Erickiller <bizarroluna@gmail.com>
2022-05-02 17:42:25 -0500

    primer commit - rest server

---
+++ README.md
@@ -0,0 +1,17 @@
+# Socket - SERVER
+
+Reconstruir mòdulos de node
+```
+npm install
+```
+
+Generar el DIST
+
+```
+tsc -w
+```
+
+Levantar servidor 
+```
+nodemon  dist/
+```
---
+++ routes/router.ts
@@ -0,0 +1,50 @@
+import {Router,Response,Request} from 'express';
+
+//para crear api end points
+const router = Router();
+
+//Controlador
+router.get('/mensajes',(req:Request,res:Response)=>{
+
+    res.json({
+        ok:true,
+        mensaje:'Todo esta bien'
+    });
+
+});
+
+
+router.post('/mensajes',(req:Request,res:Response)=>{
+
+
+    const cuerpo= req.body.cuerpo;
+    const de    = req.body.de;
+
+    res.json({
+        ok:true,
+        cuerpo,
+        de
+
+    });
+
+});
+
+
+router.post('/mensajes/:id',(req:Request,res:Response)=>{
+
+
+    const cuerpo= req.body.cuerpo;
+    const de    = req.body.de;
+    const id    = req.params.id;
+
+    res.json({
+        ok:true,
+        cuerpo,
+        de,
+        id
+
+    });
+
+});
+
+export default router;
